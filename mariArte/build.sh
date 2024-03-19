dda3bea519afacbc02b15980b72c9067fdb06266
diegolopez-dev <diego.teide@gmail.com>
2024-03-19 12:16:56 +0100

    Creando venv

--- mariArte/build.sh
+++ mariArte/build.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 pip install --upgrade pip
 
+pip install --force-reinstall -U setuptools
+
 pip install -r ./mariArte/requirements.txt
 
 python ./mariArte/manage.py collectstatic --no-input
